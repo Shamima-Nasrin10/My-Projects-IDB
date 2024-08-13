3456433734a9312927659acf545f49fc024bd58e
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-13 18:48:51 +0600

    copy

---
+++ supply-chain-management - Copy/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient(
+      withFetch()
+    )
+    
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
