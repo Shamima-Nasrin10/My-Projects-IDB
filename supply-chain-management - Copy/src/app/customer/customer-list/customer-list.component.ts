d20bae0c705a8b71a2c250981388d9c92553c6c8
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-27 19:04:39 +0600

    customer

--- supply-chain-management - Copy/src/app/customer/customer-list/customer-list.component.ts
+++ supply-chain-management - Copy/src/app/customer/customer-list/customer-list.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../model/customer.model';
+import { Order } from '../../order/model/order.model';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
-export class CustomerListComponent {
+export class CustomerListComponent implements OnInit{
+
+  customers: Customer[] = [];
+    selectedCustomer: Customer | null = null;
+    isEditing: boolean = false;
+    newOrder: Order | null = null;
+
+    constructor(private customerService: CustomerService) { }
+
+    ngOnInit(): void {
+        this.getCustomers();
+    }
+
+    getCustomers(): void {
+        this.customerService.getCustomers().subscribe(customers => this.customers = customers);
+    }
+
+    selectCustomer(customer: Customer): void {
+        this.selectedCustomer = customer;
+        this.isEditing = true;
+    }
+
+    addCustomer(): void {
+        // this.selectedCustomer = new Customer(0, '', '', '', '', '');
+        this.selectedCustomer=new Customer();
+        this.isEditing = true;
+    }
+
+    saveCustomer(customer: Customer): void {
+        if (customer.id) {
+            this.customerService.updateCustomer(customer).subscribe(() => this.getCustomers());
+        } else {
+            this.customerService.createCustomer(customer).subscribe(() => this.getCustomers());
+        }
+        this.isEditing = false;
+        this.selectedCustomer = null;
+    }
+
+    deleteCustomer(customer: Customer): void {
+        if (confirm(`Are you sure you want to delete ${customer.name}?`)) {
+            this.customerService.deleteCustomer(customer.id).subscribe(() => this.getCustomers());
+        }
+    }
+
+    cancel(): void {
+        this.isEditing = false;
+        this.selectedCustomer = null;
+    }
+
+    addOrderToCustomer(customer: Customer): void {
+        if (this.newOrder) {
+            this.customerService.addOrderToCustomer(customer.id, this.newOrder).subscribe(updatedCustomer => {
+                customer.orders = updatedCustomer.orders;
+                this.newOrder = null;
+            });
+        }
+    }
 
 }
--- supply-chain-management - Copy/src/app/customer/model/customer.model.ts
+++ supply-chain-management - Copy/src/app/customer/model/customer.model.ts
@@ -1,22 +1,22 @@
 import { Order } from "../../order/model/order.model";
 
 export class Customer {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-    address: string;
-    password: string; // For login purposes
-    orders: Order[];  // Array to hold orders of this customer
+    id!: number;
+    name!: string;
+    email!: string;
+    phone!: string;
+    address!: string;
+    password!: string; // For login purposes
+    orders!: Order[];  // Array to hold orders of this customer
 
-    constructor(id: number, name: string, email: string, phone: string, address: string, password: string, orders: Order[] = []) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.address = address;
-        this.password = password;
-        this.orders = orders;
-    }
+    // constructor(id: number, name: string, email: string, phone: string, address: string, password: string, orders: Order[] = []) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.email = email;
+    //     this.phone = phone;
+    //     this.address = address;
+    //     this.password = password;
+    //     this.orders = orders;
+    // }
 }
 
