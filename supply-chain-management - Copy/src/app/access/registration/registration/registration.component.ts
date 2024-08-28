7772ab6887a7a81cabe466b2eea56cd3623a9eb2
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-28 18:57:58 +0600

    registration

--- supply-chain-management - Copy/src/app/access/registration/registration/registration.component.ts
+++ supply-chain-management - Copy/src/app/access/registration/registration/registration.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../authentication/auth.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserModel } from '../../userModel/user.model';
 
 @Component({
   selector: 'app-registration',
@@ -12,16 +13,36 @@ export class RegistrationComponent {
 
   regForm!: FormGroup;
 
-constructor(
-  private authService: AuthService,
-  private router: Router,
-  private formBuilder: FormBuilder
-){
-  this.regForm=this.formBuilder.group({
-    name:[''],
-    email:[''],
-    password:[''],
-  })
-}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.regForm = this.formBuilder.group({
+
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(): void {
+    if (this.regForm.valid) {
+      const user: UserModel = this.regForm.value;
+      this.authService.registration(user).subscribe({
+        next: (res) => {
+          console.log('User registered successfully:', res);
+          this.authService.storeToken(res.token);
+          this.router.navigate(['/']); // Navigate to a protected route after registration
+        },
+        error: (err) => {
+          console.error('Error registering user:', err);
+        }
+      });
+    }
+    else{
+      alert("Complte mandatory Field");
+    }
+  }
 
 }
