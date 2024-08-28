19f02d0965426308d12b2f8deb7fdc405f442a6f
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-28 18:53:35 +0600

    auth

--- supply-chain-management - Copy/src/app/access/registration/registration/registration.component.ts
+++ supply-chain-management - Copy/src/app/access/registration/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../authentication/auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+
+  regForm!: FormGroup;
+
+constructor(
+  private authService: AuthService,
+  private router: Router,
+  private formBuilder: FormBuilder
+){
+  this.regForm=this.formBuilder.group({
+    name:[''],
+    email:[''],
+    password:[''],
+  })
+}
 
 }
