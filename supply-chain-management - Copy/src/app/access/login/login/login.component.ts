18feae984715e2e6f3bd6c4fcd8d01ba102d3adf
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-29 14:21:36 +0600

    login

--- supply-chain-management - Copy/src/app/access/login/login/login.component.ts
+++ supply-chain-management - Copy/src/app/access/login/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../../authentication/auth.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const credentials = this.loginForm.value;
+      this.authService.login(credentials).subscribe({
+        next: (res) => {
+          console.log('User logged in successfully:', res);
+          this.authService.storeToken(res.token);
+
+          const role=this.authService.getUserRole();
+
+          this.router.navigate(['/userprofile']);
+
+
+
+          // Navigate to a protected route after login
+        },
+        error: (error) => {
+          console.error('Error logging in:', error);
+        }
+      });
+    }
+  }
 
 }
