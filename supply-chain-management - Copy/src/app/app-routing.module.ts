5bdd93bc795e73a301f66496eef52b851c4a8b9f
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-08-31 14:11:46 +0600

    hhh

--- supply-chain-management - Copy/src/app/app-routing.module.ts
+++ supply-chain-management - Copy/src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
     path:"login", component:LoginComponent
   },
   {
-    path:'**', redirectTo:'login', pathMatch:'full'
+    path:'', redirectTo:'login', pathMatch:'full'
   },
   {
     path:"logout",component:LogoutComponent
