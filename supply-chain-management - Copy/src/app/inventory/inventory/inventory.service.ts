d544e8003f614d820dd738993c8b74dd2b3fed57
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-09-25 18:30:25 +0600

    changes

--- supply-chain-management - Copy/src/app/inventory/inventory/inventory.service.ts
+++ supply-chain-management - Copy/src/app/inventory/inventory/inventory.service.ts
@@ -8,38 +8,38 @@ import {Inventory} from "./model/inventory.model";
   providedIn: 'root'
 })
 export class InventoryService {
-  private apiUrl = 'http://localhost:8080/api/inventory';  // Base API URL
+  private apiUrl = 'http://localhost:8080/api/inventory';
 
   constructor(private http: HttpClient) {}
 
-  // Save a new inventory for a specific warehouse
+
   saveInventory(inventory: Inventory, warehouseId: number): Observable<ApiResponse> {
     const params = new HttpParams().set('warehouseId', warehouseId.toString());
     return this.http.post<ApiResponse>(`${this.apiUrl}/save`, inventory, { params });
   }
 
-  // Get all inventories
+
   getAllInventories(): Observable<ApiResponse> {
     return this.http.get<ApiResponse>(`${this.apiUrl}/list`);
   }
 
-  // Get a specific inventory by its ID
+
   getInventoryById(id: number): Observable<ApiResponse> {
     return this.http.get<ApiResponse>(`${this.apiUrl}/${id}`);
   }
 
-  // Update an existing inventory for a specific warehouse
+
   updateInventory(inventory: Inventory, warehouseId: number): Observable<ApiResponse> {
     const params = new HttpParams().set('warehouseId', warehouseId.toString());
     return this.http.put<ApiResponse>(`${this.apiUrl}/update`, inventory, { params });
   }
 
-  // Delete an inventory by its ID
+
   deleteInventoryById(id: number): Observable<ApiResponse> {
     return this.http.delete<ApiResponse>(`${this.apiUrl}/delete/${id}`);
   }
 
-  // Get all products for a specific inventory by inventory ID
+ 
   getProductsByInventoryId(inventoryId: number): Observable<ApiResponse> {
     return this.http.get<ApiResponse>(`${this.apiUrl}/${inventoryId}/products`);
   }
