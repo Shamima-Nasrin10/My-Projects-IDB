6d4763ebf52eddf883817e127a6d3612b9c0e751
Shamima-Nasrin10 <shamima48nasrin@gmail.com>
2024-09-26 12:22:58 +0600

    Error

--- supply-chain-management - Copy/src/app/inventory/inventory/inventory-create/inventory-create.component.ts
+++ supply-chain-management - Copy/src/app/inventory/inventory/inventory-create/inventory-create.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {Inventory, InventoryCategory} from "../model/inventory.model";
 import { NotifyUtil } from '../../../util/notify.util';
 import {ApiResponse} from "../../../util/api.response";
 import {InventoryService} from "../inventory.service";
 import {Product} from "../../../product/model/product.model";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Inventory} from "../model/inventory.model";
+import {WareHouse} from "../../../warehouse/warehouse/warehouse.model";
+import {WarehouseService} from "../../../warehouse/warehouse.service";
 
 @Component({
   selector: 'app-inventory-create',
@@ -13,99 +16,153 @@ import {Product} from "../../../product/model/product.model";
 export class InventoryCreateComponent implements OnInit{
   inventories: Inventory[] = [];
   inventory: Inventory = new Inventory();
+  warehouses: WareHouse[] = [];
   products: Product[] = [];
-  selectedInventoryId!: number;
-  isEditMode: boolean = false;
+  inventoryId?: number;
+  selectedWarehouseId!: number;
 
-  constructor(private inventoryService: InventoryService) { }
+  dynamicClasses = {
+    'dynamic-background': true,
+    'dynamic-border': true
+  };
+
+  constructor(
+    private inventoryService: InventoryService,
+    private warehouseService: WarehouseService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    // Ensure that inventory.warehouse is initialized as an empty WareHouse object
+    this.inventory.warehouse = new WareHouse();
+
     this.loadInventories();
+    this.loadWarehouses();
   }
 
-
-  loadInventories(): void {
-    this.inventoryService.getAllInventories().subscribe((response: ApiResponse) => {
-      if (response.success) {
-        this.inventories = response.data['inventories'];
-      } else {
-        NotifyUtil.error(response.message);
+  public loadInventories(): void {
+    this.inventoryService.getAllInventories().subscribe({
+      next: (response: ApiResponse) => {
+        if (response.success) {
+          this.inventories = response.data['inventories'];
+        } else {
+          NotifyUtil.error(response.message);
+        }
+      },
+      error: (error) => {
+        NotifyUtil.error(error);
       }
-    }, (error) => {
-      NotifyUtil.error(error);
     });
   }
 
-  saveInventory(): void {
-    if (this.isEditMode && this.selectedInventoryId) {
-      this.inventoryService.updateInventory(this.inventory, this.inventory.warehouse.id).subscribe((response: ApiResponse) => {
+  public loadWarehouses(): void {
+    this.warehouseService.getAllWarehouses().subscribe({
+      next: (response: ApiResponse) => {
         if (response.success) {
-          NotifyUtil.success(response);
-          this.loadInventories();
-          this.clearForm();
+          this.warehouses = response.data['warehouses'];
         } else {
           NotifyUtil.error(response.message);
         }
-      }, (error) => {
+      },
+      error: (error) => {
         NotifyUtil.error(error);
-      });
-    } else {
-
-      this.inventoryService.saveInventory(this.inventory, this.inventory.warehouse.id).subscribe((response: ApiResponse) => {
-        if (response.success) {
-          NotifyUtil.success(response);
-          this.loadInventories();
-          this.clearForm();
-        } else {
-          NotifyUtil.error(response.message);
-        }
-      }, (error) => {
-        NotifyUtil.error(error);
-      });
-    }
-  }
-
-  editInventory(inventory: Inventory): void {
-    this.isEditMode = true;
-    this.inventory = { ...inventory };
-    this.selectedInventoryId = inventory.id;
-  }
-
-  deleteInventory(id: number): void {
-    if (confirm("Are you sure you want to delete this inventory?")) {
-      this.inventoryService.deleteInventoryById(id).subscribe((response: ApiResponse) => {
-        if (response.success) {
-          NotifyUtil.success(response);
-          this.loadInventories();
-        } else {
-          NotifyUtil.error(response.message);
-        }
-      }, (error) => {
-        NotifyUtil.error(error);
-      });
-    }
-  }
-
-  onSelectInventory(inventoryId: number): void {
-    this.selectedInventoryId = inventoryId;
-    this.inventoryService.getProductsByInventoryId(inventoryId).subscribe((response: ApiResponse) => {
-      if (response.success) {
-        this.products = response.data['products'];
-      } else {
-        NotifyUtil.error(response.message);
       }
-    }, (error) => {
-      NotifyUtil.error(error);
     });
   }
 
-  clearForm(): void {
+  public loadProductsByInventoryId(inventoryId: number): void {
+    this.inventoryService.getProductsByInventoryId(inventoryId).subscribe({
+      next: (response: ApiResponse) => {
+        if (response.success) {
+          this.products = response.data['products'];
+        } else {
+          NotifyUtil.error(response.message);
+        }
+      },
+      error: (error) => {
+        NotifyUtil.error(error);
+      }
+    });
+  }
+
+  public onSubmit(): void {
+    if (!this.inventory.warehouse || !this.inventory.warehouse.id) {
+      NotifyUtil.error('Please select a warehouse before saving the inventory.');
+      return;
+    }
+
+    const inventoryObservable = this.inventoryId
+      ? this.inventoryService.updateInventory(this.inventory, this.inventory.warehouse.id)
+      : this.inventoryService.saveInventory(this.inventory, this.inventory.warehouse.id);
+
+    inventoryObservable.subscribe({
+      next: (response: ApiResponse) => {
+        if (response.success) {
+          this.resetInventoryForm();
+          NotifyUtil.success(response);
+        } else {
+          NotifyUtil.error(response.message);
+        }
+      },
+      error: (error) => {
+        NotifyUtil.error(error);
+      }
+    });
+  }
+
+  public resetInventoryForm(): void {
     this.inventory = new Inventory();
-    this.isEditMode = false;
-    this.selectedInventoryId = 0;
+    this.inventory.warehouse = new WareHouse();  // Initialize warehouse again when resetting the form
+    this.loadInventories();
     this.products = [];
+    this.inventoryId = undefined;
+    this.selectedWarehouseId = 0;
   }
 
-  protected readonly Inventory = Inventory;
-  protected readonly InventoryCategory = InventoryCategory;
+  public deleteInventory(id: number): void {
+    if (confirm('Are you sure you want to delete this inventory?')) {
+      this.inventoryService.deleteInventoryById(id).subscribe({
+        next: (response: ApiResponse) => {
+          if (response.success) {
+            this.resetInventoryForm();
+            NotifyUtil.success(response);
+          } else {
+            NotifyUtil.error(response.message);
+          }
+        },
+        error: (error) => {
+          NotifyUtil.error(error);
+        }
+      });
+    }
+  }
+
+  public editInventory(inventory: Inventory): void {
+    this.inventory = { ...inventory };
+    this.inventoryId = inventory.id;
+
+    if (!this.inventory.warehouse) {
+      this.inventory.warehouse = new WareHouse();  // Ensure the warehouse is initialized
+    }
+
+    this.loadProductsByInventoryId(inventory.id);
+  }
+
+  public onSelectInventory(inventoryId: number): void {
+    this.inventoryService.getInventoryById(inventoryId).subscribe({
+      next: (response: ApiResponse) => {
+        if (response.success) {
+          this.inventory = response.data['inventory'];
+          this.inventoryId = inventoryId;
+          this.loadProductsByInventoryId(inventoryId); // Load products for the selected inventory
+        } else {
+          NotifyUtil.error(response.message);
+        }
+      },
+      error: (error) => {
+        NotifyUtil.error(error);
+      }
+    });
+  }
 }
