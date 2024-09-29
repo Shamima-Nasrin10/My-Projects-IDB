ca2102de1265e0d9682306b18817904c47b1af40
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-09-29 15:13:59 +0600

    salesDetails

--- supply-chain-management - Copy/src/app/accounting/sales/model/sales.model.ts
+++ supply-chain-management - Copy/src/app/accounting/sales/model/sales.model.ts
@@ -1,3 +1,14 @@
-export class Sales{
+import {Retailer} from "../../../product/retailer/model/retailer.model";
+import {Product} from "../../../product/model/product.model";
+import {SalesDetails} from "./salesdetails.model";
 
+export class Sales{
+  id!: number;
+  retailer!: Retailer;
+  salesdate!: Date;
+  totalprice!: number;
+  quantity!: number;
+  discount!: number;
+  product!: Product[];
+  salesDetails!: SalesDetails;
 }
